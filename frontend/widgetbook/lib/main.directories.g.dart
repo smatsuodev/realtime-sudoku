21ab858f97777d38e4d9bc65d42101ac5310d47e
EringiShimeji <eringi.shimeji.5296@gmail.com>
2024-08-14 00:44:51 +0900

    RoomsListPageを作成

--- frontend/widgetbook/lib/main.directories.g.dart
+++ frontend/widgetbook/lib/main.directories.g.dart
@@ -11,6 +11,8 @@
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:widgetbook/widgetbook.dart' as _i1;
 import 'package:widgetbook_workspace/examples/cool_button.dart' as _i2;
+import 'package:widgetbook_workspace/presentation/page/rooms_list_page.dart'
+    as _i3;
 
 final directories = <_i1.WidgetbookNode>[
   _i1.WidgetbookFolder(
@@ -24,5 +26,22 @@ final directories = <_i1.WidgetbookNode>[
         ),
       )
     ],
-  )
+  ),
+  _i1.WidgetbookFolder(
+    name: 'presentation',
+    children: [
+      _i1.WidgetbookFolder(
+        name: 'page',
+        children: [
+          _i1.WidgetbookLeafComponent(
+            name: 'RoomsListPage',
+            useCase: _i1.WidgetbookUseCase(
+              name: 'Deafult',
+              builder: _i3.buildCoolButtonUseCase,
+            ),
+          )
+        ],
+      )
+    ],
+  ),
 ];
