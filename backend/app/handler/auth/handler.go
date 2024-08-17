8afa0e216a2f7143baa96d7cb40c2187babd2ba4
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-08-18 01:21:43 +0900

    handler SignIn

--- backend/app/handler/auth/handler.go
+++ backend/app/handler/auth/handler.go
@@ -3,6 +3,7 @@ package auth
 import (
 	"connectrpc.com/connect"
 	"context"
+	"net/http"
 	authv1 "sudoku/gen/sudoku/auth/v1"
 	"sudoku/service/auth"
 )
@@ -18,8 +19,26 @@ func NewHandler(authService auth.IAuthService) *Handler {
 }
 
 func (h *Handler) SignIn(ctx context.Context, req *connect.Request[authv1.SignInRequest]) (*connect.Response[authv1.SignInResponse], error) {
-	//TODO implement me
-	panic("implement me")
+	output, err := h.authService.SignIn(auth.SignInInput{
+		Provider: auth.OAuthProvider(req.Msg.Provider),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO: 属性を追加
+	cookie := http.Cookie{
+		Name:  "state_jwt",
+		Value: output.StateJWT,
+	}
+	// Cookie として返していいのか?
+	req.Header().Set("Set-Cookie", cookie.String())
+
+	res := connect.NewResponse(&authv1.SignInResponse{
+		AuthorizationUrl: output.AuthorizationURL,
+	})
+
+	return res, nil
 }
 
 func (h *Handler) SignOut(ctx context.Context, req *connect.Request[authv1.SignOutRequest]) (*connect.Response[authv1.SignOutResponse], error) {
