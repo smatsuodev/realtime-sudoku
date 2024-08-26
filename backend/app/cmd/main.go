8b96f6f2fab0a8a8d2305568fee2e409686d78d3
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-08-27 02:06:32 +0900

    一定時間ごとに期限切れのセッションを削除するジョブを実行

--- backend/app/cmd/main.go
+++ backend/app/cmd/main.go
@@ -5,9 +5,12 @@ import (
 	"log"
 	"sudoku/config"
 	"sudoku/infra/gorm"
+	"sudoku/job"
 	"sudoku/server"
+	"time"
 )
 
+// NOTE: ctx を正しく使えてない気がする
 func main() {
 	cfg := config.NewEnvConfig()
 
@@ -18,6 +21,12 @@ func main() {
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+
+	sessionCleanupJob := job.NewSessionCleanupJob(
+		gorm.NewSessionRepository(db),
+		time.Hour, // TODO: env から読む?
+	)
+	go sessionCleanupJob.Run(ctx)
 
 	server.Run(ctx, cfg, db)
 }
